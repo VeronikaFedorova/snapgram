e3120524d8041e9df3c20c52abb9ee27ec853c5c
Veronika Fedorova <fedorovavv03@gmail.com>
2024-09-13 18:15:27 +0300

    Initialise a new project

---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+const SigninForm = () => {
+  return (
+    <Button>MMMMMMMMMMM</Button>
+  )
+}
+
+export default SigninForm
---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+const RootLayout = () => {
+  return (
+    <></>
+  )
+}
+
+export default RootLayout
